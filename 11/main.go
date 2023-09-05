67d444e68ab19b0bea5537922846242dd25e52d0
samuelfsd <samuel.estudo778@gmail.com>
2023-09-05 08:47:47 -0300

    feat: more about structs

--- 11/main.go
+++ 11/main.go
@@ -4,23 +4,35 @@ import "fmt"
 
 // entendendo uma struct
 // semelhante a uma classe pórem diferente
+type Address struct {
+	city   string
+	number int
+	state  string
+}
 
-type Cliente struct {
+// structs podem ter composições
+// ou encadeamento de structs
+type Client struct {
 	name     string
 	age      int
 	isActive bool
+	Address
 }
 
 func main() {
 
-	marlon := Cliente{
+	marlon := Client{
 		name:     "Marlon",
 		age:      30,
 		isActive: true,
 	}
 
-  // é possivel mudar atributos
-  marlon.isActive = false
+	// é possivel mudar atributos
+	marlon.isActive = false
 	fmt.Println(marlon.name)
 	fmt.Printf("Nome ->  %s \nIdade -> %d \nÉ ativo -> %t \n", marlon.name, marlon.age, marlon.isActive)
+
+	marlon.city = "Paulista PB"
+
+	fmt.Println(marlon.city)
 }
