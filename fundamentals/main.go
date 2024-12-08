fa283e28eb383f91a92d901ff1dce62416fdf70e
samuelfsd <samuel.estudo778@gmail.com>
2024-12-08 12:21:54 -0300

    fix: remove comentários

--- fundamentals/main.go
+++ fundamentals/main.go
@@ -2,9 +2,6 @@ package main
 
 import "fmt"
 
-// entendendo pacotes no go -> package
-// tudo que está dentro do mesmo package pode ser acessado sem precisar importar
-
 func main() {
 	fmt.Println("Olá mundo!")
 }
